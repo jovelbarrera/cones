0b09636edd55456100ede2a2669ad0f5ff1ec1b5
Jaime García <jg0592@gmail.com>
2015-06-13 18:25:26 -0600

    Mas patrocinadores

--- application/views/preinscrip_exito.php
+++ application/views/preinscrip_exito.php
@@ -1,4 +1,5 @@
 <div class="alert alert-dismissible alert-success">
   <button type="button" class="close" data-dismiss="alert">×</button>
-  <strong>Gracias por preinscribirte!</strong> verifica tu correo para confirmar tu preinscrición.
+  <strong>Gracias por preinscribirte!</strong> 
+  <!-- verifica tu correo para confirmar tu preinscrición. -->
 </div>
